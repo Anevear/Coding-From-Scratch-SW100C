b8a3b9035df2d0829d52a6e16650991ba743a3a9
Anevear <87884506+Anevear@users.noreply.github.com>
2021-08-01 11:10:10 -0500

    Working
    Likely just me moving files around and creating new ones as I go thru my lessons

---
+++ HTML Ref/arrayAct.js
@@ -0,0 +1,12 @@
+var amazingCars = ["BMW", "Lexus", "Ford", "Mercedes"]
+console.log(amazingCars);
+
+amazingCars.pop();
+
+var sort = amazingCars.sort();
+amazingCars.sort();
+console.log(sort);
+
+var length = amazingCars.length;
+amazingCars.length;
+console.log(length);
